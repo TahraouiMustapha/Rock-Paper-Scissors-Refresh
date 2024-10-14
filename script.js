cdb56c1aad19cebea36de801166e1c2638cd9875
mustapha <moustaphaoussama1818@gmail.com>
2024-10-14 20:40:08 +0100

    feat: Adding logic for announce a winner of the game once one player reaches 5 points

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const choices = ['rock', 'paper', 'scissors'];
 let humanScore = 0, computerScore = 0;
+updateScores();
 
 function getComputerChoice () {
     let randomIndex = Math.floor(Math.random() * 3);
@@ -9,6 +10,19 @@ function getComputerChoice () {
 function displayResult (resultOfRound) {
     const resultDiv = document.querySelector('.result');
     resultDiv.textContent = resultOfRound;
+}
+
+function updateScores() {
+    const humanScoreDiv = document.querySelector('.human.score');
+    const computerScoreDiv = document.querySelector('.computer.score');
+
+    humanScoreDiv.textContent = humanScore;
+    computerScoreDiv.textContent = computerScore;
+}
+
+function resetScores() {
+    humanScore = 0;
+    computerScore = 0;
 }
 // function to check if the human is the winner
 function isWin(humanChoice, computerChoice) {
@@ -26,10 +40,22 @@ function playRound (humanChoice, computerChoice) {
         displayResult('draw');
     } else if ( isWin(humanChoice, computerChoice) ) {
         humanScore++;
-        displayResult(`You Win! ${humanChoice} beats ${computerChoice}`);
+        updateScores();
+        if(humanScore == 5) {
+            displayResult('You Win The Game !!!!!');
+            resetScores();
+        } else {
+            displayResult(`You Win! ${humanChoice} beats ${computerChoice}`);
+        }
     } else {
         computerScore++;
-        displayResult(`You Lose! ${computerChoice} beats ${humanChoice}`);
+        updateScores();
+        if(computerScore == 5) {
+            displayResult('You Lose The Game <->');
+            resetScores();
+        } else {
+            displayResult(`You Lose! ${computerChoice} beats ${humanChoice}`);
+        }    
     }
 }
 
